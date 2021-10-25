5da209fad8499110afbb5593f388eaf8a4ab1211
Malika <malikasenova@gmail.com>
2021-10-25 14:28:46 -0700

    Add navlink to the edit icon

--- app/src/Flashcards/Flashcard.js
+++ app/src/Flashcards/Flashcard.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-// import { ModeEdit } from "@mui/icons-material/ModeEdit";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
+import { NavLink } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
@@ -16,9 +16,11 @@ const Flashcard = ({ flashcard }) => {
       <Stack spacing={2} direction="row" className={styles.stack}>
         <div className={styles.sideList}>{flashcard.front_of_card}</div>
         <div className={styles.sideList}>{flashcard.back_of_card}</div>
-        <IconButton to="create-edit-card" aria-label="edit the flashcard">
-          <EditIcon sx={{ fontSize: 40 }} />
-        </IconButton>
+        <NavLink to="create-edit-card">
+          <IconButton to="create-edit-card" aria-label="edit the flashcard">
+            <EditIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        </NavLink>
       </Stack>
     </>
   );
