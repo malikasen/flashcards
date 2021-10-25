c9d0a266b8e314f8c068b049e0c26f5aba821c2b
Malika <malikasenova@gmail.com>
2021-10-25 15:28:35 -0700

    Place blank sides and buttons inside a form

--- app/src/Flashcards/EmptySides.js
+++ app/src/Flashcards/EmptySides.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { useState } from "react";
 
+import AddToPhotosIcon from "@mui/icons-material/AddToPhotos";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
 import styles from "./styles.module.scss";
@@ -9,18 +13,35 @@ const EmptySides = () => {
   const [front, setFront] = useState("");
   const [back, setBack] = useState("");
   return (
-    <Stack spacing={2} direction="column" className={styles.stack2}>
-      <input
-        className={styles.side}
-        value={front}
-        onChange={(e) => setFront(e.target.value)}
-      ></input>
-      <input
-        className={styles.side}
-        value={back}
-        onChange={(e) => setBack(e.target.value)}
-      ></input>
-    </Stack>
+    <>
+      <form>
+        <Stack spacing={2} direction="row" className={styles.stack}>
+          <Button variant="outlined" startIcon={<DeleteIcon />}>
+            Delete
+          </Button>
+          <Button variant="outlined" startIcon={<SaveAltIcon />}>
+            Save
+          </Button>
+          <Button variant="outlined" startIcon={<AddToPhotosIcon />}>
+            Save and add more
+          </Button>
+        </Stack>
+        <Stack spacing={2} direction="column" className={styles.stack2}>
+          <input
+            className={styles.side}
+            value={front}
+            onChange={(e) => setFront(e.target.value)}
+            required
+          ></input>
+          <input
+            className={styles.side}
+            value={back}
+            onChange={(e) => setBack(e.target.value)}
+            required
+          ></input>
+        </Stack>
+      </form>
+    </>
   );
 };
 export default EmptySides;
