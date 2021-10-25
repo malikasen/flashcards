11c3e1dd9c4c0d489e6927c4b89b113d08710b81
Malika <malikasenova@gmail.com>
2021-10-25 12:09:36 -0700

    Add edit icons in the home page

--- app/src/Flashcards/Flashcard.js
+++ app/src/Flashcards/Flashcard.js
@@ -1,10 +1,22 @@
 import * as React from "react";
 
+// import { ModeEdit } from "@mui/icons-material/ModeEdit";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+
+import styles from "./styles.module.scss";
+
 const Flashcard = ({ flashcard }) => {
   return (
     <>
-      <div>{flashcard.front_of_card}</div>
-      <div>{flashcard.back_of_card}</div>
+      <Stack spacing={2} direction="row" className={styles.stack}>
+        <div className={styles.sideList}>{flashcard.front_of_card}</div>
+        <div className={styles.sideList}>{flashcard.back_of_card}</div>
+        <IconButton aria-label="edit the flashcard">
+          <EditIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+      </Stack>
     </>
   );
 };
