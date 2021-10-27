11f7b1d2d9d6bc737083500fa716acb893439901
Malika <malikasenova@gmail.com>
2021-10-27 08:44:53 -0700

    Test deployment

--- app/src/App/index.js
+++ app/src/App/index.js
@@ -82,6 +82,7 @@ const Home = ({ flashcards, loading }) => {
       {isAuthenticated && !loading ? (
         <div>
           <h2>Hello, {user.given_name}</h2>
+          <h2>Deployment test</h2>
           <Flashcards flashcards={flashcards} />
         </div>
       ) : null}
