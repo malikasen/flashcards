5f6c04deb60d14b8f43c7340f95575c72e45fb45
Malika <malikasenova@gmail.com>
2021-11-03 23:06:45 -0700

    Format practice card

--- app/src/Flashcards/Side.js
+++ app/src/Flashcards/Side.js
@@ -1,22 +1,21 @@
 import * as React from "react";
 
 import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
 
 import styles from "./styles.module.scss";
 
 const Side = ({ text, toggleSide }) => {
   return (
-    <Stack spacing={2} direction="row" className={styles.stack}>
+    <div className={styles.practiceCardContainer}>
       <Button
         variant="outlined"
         size="large"
-        className={styles.side}
+        className={styles.practiceCard}
         onClick={toggleSide}
       >
         {text}
       </Button>
-    </Stack>
+    </div>
   );
 };
 export default Side;
