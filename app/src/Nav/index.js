81c5282601f9bd43eb7e04ffd9eff1aed52e6c05
Malika <malikasenova@gmail.com>
2021-11-11 12:20:24 -0800

    Add logo to navbar and footer

--- app/src/Nav/index.js
+++ app/src/Nav/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import useAuth0 from "../auth/useAuth0";
 import { Login, Logout } from "../auth/widgets";
+import logo from "../images/Pomni-logos_transparent.png";
 
 import { Nav, NavLink, Bars, NavMenu, NavBtn } from "./NavbarElements";
 import styles from "./styles.module.scss";
@@ -9,7 +10,7 @@ import styles from "./styles.module.scss";
 const Navigation = ({ toggle }) => (
   <Nav>
     <NavLink to="/" end>
-      <h3 className={styles.appName}>Pomni</h3>
+      <img className={styles.logo} src={logo} alt="Pomni logo"></img>
     </NavLink>{" "}
     <Bars onClick={toggle} />
     <NavMenu>
